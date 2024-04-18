5c3d9779ac6c154bb90ab1307aa86567ea77426e
lightshadowL <106723656+lightshadowL@users.noreply.github.com>
2024-04-18 12:05:32 +0900

    basic

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Footer from './Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Info from './pages/Info';
+import State from './pages/State';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/info' element={<Info/>}/>
+          <Route path='/state' element={<State/>}/>
+        </Routes>
+      </BrowserRouter>
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
